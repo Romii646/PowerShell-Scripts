82783e101275bbc8b13124dcd2e372f108184bdd
Romii646 <cortinacort@gmail.com>
2025-06-23 21:15:24 -0400

    Fixed path way for script execution via PDQ

--- Rename/Schedule-Execute-Rename.ps1
+++ Rename/Schedule-Execute-Rename.ps1
@@ -1,11 +1,11 @@
-# Path to copy the renaming script to
-$targetPath = "$env:ProgramData\Rename-Asset-BySerial.ps1"
+# FolderPath is used to copy the script to the target system
+$folderPath = "C:Users\Public\Desktop"
 
-# copy script from PDQ working directory to local path
+# Path to copy the renaming script to
+$targetPath = Join-Path $folderPath "Rename-Asset-BySerial.ps1" # Copies Rename-PC-BySerial.ps1 from the PDQ working directory where the deploy step runs to the target system.
+
+# copy script from PDQ working directory to local path ensure that we have a full path for -path
 Copy-Item -Path "Rename-Asset-BySerial.ps1" -Destination $targetPath -Force
 
-#Register the script to run once at startup
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -Name "RenameAssetBySerial" -Value $targetPath -Force
-    -Name "RenamePCOnce" `
-    -Value "PowerShell.exe -ExecutionPolicy Bypass -File `"$targetPath`"" `
-    -PropertyType String -Force
+# Register the script to run once at startup
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -Name "RenameAssetBySerial" -Value "PowerShell.exe -ExecutionPolicy Bypass -File `"$targetPath`"" -PropertyType String -Force
